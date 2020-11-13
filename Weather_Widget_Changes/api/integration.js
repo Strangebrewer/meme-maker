059f20610fb3627c9e993d4b6fdff8befe3a1a4e
Keith Allmon <keith.allmon@trudigital.net>
2020-11-13 10:43:23 -0700

    wtf

--- Weather_Widget_Changes/api/integration.js
+++
@@ -1,8 +0,0 @@
-import axios from '../plugins/axios';
-
-const endpoint = 'integrations';
-
-export default {
-    stock: (data) => axios.get(`${endpoint}/stock?symbol=${data}`),
-    weather: data => axios.get(`${endpoint}/weather?zipcode=${data.zipcode}&metric=${data.metric}`),
-};
